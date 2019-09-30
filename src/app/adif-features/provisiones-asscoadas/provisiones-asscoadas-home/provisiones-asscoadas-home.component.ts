4874f1a5e3ec4366f40316904cd7e54ddb8d91ef
chdattat <chillal.dattatreya@capgemini.com>
2019-09-30 16:50:54 +0530

    Cont Prov Frontend Latest

---
+++ src/app/adif-features/provisiones-asscoadas/provisiones-asscoadas-home/provisiones-asscoadas-home.component.ts
@@ -0,0 +1,208 @@
+import { Component, OnInit, ViewChild, HostListener, OnDestroy } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { Observable, Subject, interval } from 'rxjs';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import * as moment from 'moment';
+import { takeUntil, distinctUntilChanged, debounceTime, tap, switchMap, throttleTime } from 'rxjs/internal/operators';
+import { ProvisionesDatasource } from '../service/provisiones.datasource';
+import { ProvisionesAsscoadasService,
+        SearchProvisiones,
+        ProvisionesContent,
+        ProvisionesAsscodas,
+        ResponseProvisionesAsscodas,
+        SearchProvisionesContent} from '../service/provisiones-asscoadas.service';
+import { DataTableComponent, PageQuery } from '../../../shared/adif-data-table/data-table/data-table.component';
+import { HttpErrorResponse } from '@angular/common/http';
+import { NotificationService } from '../../../shared/notification/notification.service';
+import { TranslationES } from '../../../shared/translation/translate_es';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'adif-provisiones-asscoadas-home',
+  templateUrl: './provisiones-asscoadas-home.component.html',
+  styleUrls: ['./provisiones-asscoadas-home.component.scss']
+})
+export class ProvisionesAsscoadasHomeComponent implements OnInit, OnDestroy {
+  provisionesPeriodo: any;
+  private destroyed$ = new Subject();
+  private readonly debounceTimeInMillis = 300;
+  dataSourceSelected = false;
+  private periodo: Params;
+  private initialPage: PageQuery = {
+    pageIndex: 0, pageSize: 5
+  };
+  private unsubscribe = new Subject();
+  datalength: number;
+  provisionesSearch;
+  provisionesSearchForm: FormGroup;
+  @ViewChild(DataTableComponent) dataTab: DataTableComponent;
+  dataTableSource: Observable<ResponseProvisionesAsscodas[]>;
+  dataSource: ProvisionesDatasource;
+  loading = false;
+  columnsToDisplay = ['radio', 'codigo', 'cod_sociedad'];
+  columnsParams = {codigo: 'Codigo Sap Expediente', cod_sociedad: 'Cod Sociedad'};
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private route: ActivatedRoute,
+    private provisionesAsscoadasService: ProvisionesAsscoadasService,
+    private notificationService: NotificationService,
+    private toastr: ToastrService
+  ) {
+    this.route.queryParams.subscribe(queryParams => this.periodo = queryParams);
+    this.provisionesSearchForm = formBuilder.group({
+      provisionesSearch: ''
+    });
+    this.provisionesSearch = this.provisionesSearchForm.get('provisionesSearch');
+    this.provisionesSearch.valueChanges
+    .pipe(
+        takeUntil(this.unsubscribe),
+        debounceTime(this.debounceTimeInMillis),
+        distinctUntilChanged(),
+        switchMap(searchValue => {
+          this.loading = true;
+          return this.search({
+            codigo: searchValue.toString(),
+            page: this.dataTab.paginator.pageIndex,
+            size: this.dataTab.paginator.pageSize,
+            timestamp: this.periodo['periodo']
+          });
+        })
+      )
+      .subscribe((data: SearchProvisionesContent) => {
+        this.dataTab.paginator.firstPage();
+        this.dataSource.loadProvisionesData(data.content);
+        this.datalength = data.totalElements;
+        this.loading = false;
+      }, (error: HttpErrorResponse) => {
+        this.loading = false;
+        this.notificationService.setNotification(TranslationES.provisiones_contable.serviceUnAvailable);
+      });
+      this.listeningPortService();
+  }
+
+  ngOnInit() {
+    this.dataSource = new ProvisionesDatasource();
+    this.loading = true;
+    this.addAllDataToSource(this.initialPage, false);
+  }
+
+  listeningPortService() {
+    interval(300)
+    .pipe(
+        takeUntil(this.destroyed$),
+        throttleTime(3000),
+        switchMap(() => this.provisionesAsscoadasService.podListner())
+    )
+    .subscribe(data => {
+      if (data) {
+        this.toastr.success(data);
+      }
+    });
+  }
+
+  addAllDataToSource(page: PageQuery, startsWithFirst: boolean) {
+    this.provisionesAsscoadasService.listAll(this.periodo['periodo'], page)
+    .subscribe((data: ProvisionesContent) => {
+      if (data) {
+        if (startsWithFirst) {
+          this.dataTab.paginator.firstPage();
+        }
+        this.dataSource.loadProvisionesData(data.content);
+        this.provisionesPeriodo = data && data.content.length ?
+                      moment(new Date(data.content[0].key.periodo)).format('MMMM DD, YYYY') : null;
+        this.datalength = data.totalElements;
+        this.loading = false;
+      }
+    }, (error: HttpErrorResponse) => {
+      this.loading = false;
+      this.notificationService.setNotification(TranslationES.provisiones_contable.serviceUnAvailable);
+    });
+    this.dataTableSource = this.dataSource.getProvisionesData();
+  }
+
+  requestPage(pageQuery: PageQuery) {
+    this.loading = true;
+    if (!this.provisionesSearch.value) {
+      this.provisionesAsscoadasService.listAll(this.periodo['periodo'], pageQuery)
+      .subscribe((data: ProvisionesContent) => {
+        this.dataSource.loadProvisionesData(data.content);
+        this.loading = false;
+      }, (error: HttpErrorResponse) => {
+        this.loading = false;
+        this.notificationService.setNotification(TranslationES.provisiones_contable.serviceUnAvailable);
+      });
+    } else {
+      this.provisionesAsscoadasService.searchWithDate({
+        codigo: this.provisionesSearch.value,
+        page: pageQuery.pageIndex,
+        size: pageQuery.pageSize,
+        timestamp: this.periodo['periodo']
+      })
+      .subscribe((data: SearchProvisionesContent) => {
+        this.dataSource.loadProvisionesData(data.content);
+        this.loading = false;
+      }, (error: HttpErrorResponse) => {
+        this.loading = false;
+        this.notificationService.setNotification(TranslationES.provisiones_contable.serviceUnAvailable);
+      });
+    }
+  }
+
+  private search(search: SearchProvisiones): Observable<SearchProvisionesContent> {
+    return this.provisionesAsscoadasService.searchWithDate(search);
+  }
+
+  eventCaptured(event: boolean) {
+    if (event) {
+      this.dataSourceSelected = true;
+    }
+  }
+
+  private pagable() {
+    return {
+      pageIndex: this.dataTab.paginator.pageIndex,
+      pageSize: this.dataTab.paginator.pageSize
+    };
+  }
+
+  nuevo() {
+    this.router.navigate(['provisiones-asscoadas-actions'], {
+      relativeTo: this.route, queryParams: { periodo: this.periodo['periodo'] },  queryParamsHandling: 'merge'
+    });
+  }
+
+  delete() {
+    if (this.dataTab) {
+      this.loading = true;
+      const selectRow: ResponseProvisionesAsscodas = this.dataTab.selectedRow;
+      this.provisionesAsscoadasService.delete(selectRow)
+      .subscribe(done => {
+        this.addAllDataToSource(this.pagable(), false);
+        this.notificationService.setNotification(TranslationES.provisiones_contable.delete);
+      }, (error: HttpErrorResponse) => {
+        this.loading = false;
+        this.notificationService.setNotification(TranslationES.provisiones_contable.serviceUnAvailable);
+      }, () => this.dataSourceSelected = false);
+    }
+  }
+
+  @HostListener('body:click', ['$event'])
+  onclick(event) {
+    const ele: HTMLElement = <HTMLElement> event.target;
+    const eleClass = ele.parentElement.parentElement;
+    if (eleClass && !eleClass.classList.contains('notificationMsg')) {
+      this.notificationService.setNotification(null);
+    }
+  }
+
+  @HostListener('window:popstate', ['$event'])
+  onPopState(event) {
+    this.notificationService.setNotification(null);
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed$.next();
+    this.destroyed$.complete();
+  }
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  defaultLanguage: 'es-ES',
+  supportedLanguages: [
+    'es-ES'
+  ],
+provision_serverUrl: 'http://provision-grpc-adifv2.apps.okd.openshifthub.ml',
+const_serverUrl: 'http://contable-grpc-adifv2.apps.okd.openshifthub.ml',
+security: 'csrf'
+};
