6f4f011b971ed9ede1a344a49efbbc1189d5ecb6
pragathibaskar <47776430+pragathibaskar@users.noreply.github.com>
2019-10-01 17:13:02 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,19 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
-import { Subscription } from 'rxjs/internal/Subscription';
-import { filter } from 'rxjs/internal/operators/filter';
 
 @Component({
   selector: 'adif-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit, OnDestroy {
-  private _routerSub = Subscription.EMPTY;
+export class AppComponent implements OnInit {
   title = 'adif';
-  isCierresActive = true;
-  isProvisionesActionActive = false;
   showMenu = false;
   constructor(
     private router: Router
   ) {}
 
   ngOnInit() {
-    console.log(this.router.url);
-    this._routerSub = this.router.events.pipe(
-            filter(event => event instanceof NavigationEnd)
-          )
-         .subscribe((activeRoute: NavigationEnd) => {
-          !activeRoute.url.includes('provisiones-asscoadas') ? this.isCierresActive = true : this.isCierresActive = false;
-         });
-  }
-
-  ngOnDestroy() {
-    this._routerSub.unsubscribe();
   }
 
   show() {
